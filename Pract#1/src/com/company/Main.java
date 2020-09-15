268583cdf4525b6f2a23b2bb4b8840c964a6f536
kovalenko-vlm <70951109+kovalenko-vlm@users.noreply.github.com>
2020-09-15 19:30:48 +0300

    pract1

---
+++ Pract#1/src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Task2
+        int[] arr = fillArray(0, 10, 2);
+        System.out.println(arr.length);
+        printArray(arr);
+
+//        Task1
+        matrix matr = new matrix(4, 4);
+        matr.printMatrix();
+        matr.printSideDiagonal();
+
+        System.out.println();
+
+        matrix matr2 = new matrix();
+        matr2.setSize1(6);
+        matr2.setSize2(4);
+        matr2.printMatrix();
+        matr2.printSideDiagonal();
+    }
+
+    private static int[] fillArray(int begin, int end, int step){
+        int[] arr = new int[end/step];
+
+        int temp = begin;
+        for(int i = 0; i < end / step; i++){
+            arr[i] = temp;
+            temp += step;
+        }
+        return arr;
+    }
+
+    private static void printArray(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ Pract#1/src/com/company/matrix.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import java.util.Random;
+
+public class matrix {
+    private final Random random = new Random();
+    private int size1;
+    private int size2;
+    private int[][] arr;
+
+    public matrix(int size1, int size2){
+        this.size1 = size1;
+        this.size2 = size2;
+    }
+
+    public matrix(){
+    }
+
+    public int getSize1(){
+        return size1;
+    }
+
+    public void setSize1(int size1){
+        if(size1 > 0)
+            this.size1 = size1;
+        else
+            System.out.println("We need only positive numbers");
+    }
+
+    public int getSize2(){
+        return size2;
+    }
+
+    public void setSize2(int size2){
+        if(size2 > 0)
+            this.size2 = size2;
+        else
+            System.out.println("We need only positive numbers");
+    }
+
+    public void randomize(){
+        arr = new int[size1][size2];
+        for (int i = 0; i < size1; i++){
+            for(int j = 0; j < size2; j++){
+                arr[i][j] = random.nextInt(10);
+            }
+        }
+    }
+
+    public void printMatrix(){
+        randomize();
+        for (int i = 0; i < size1; i++){
+            for(int j = 0; j < size2; j++){
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void printSideDiagonal(){
+        if(size1 == size2){
+            int size = size2;
+            for (int i = 0; i < size1; i++){
+                for(int j = size - 1; j >= 0; j--){
+                    System.out.print(arr[i][j] + " ");
+                    break;
+                }
+                size -= 1;
+            }
+            System.out.println();
+        } else {
+            System.out.print("We can`t do this dude! Number of rows must be equal to number of cols");
+        }
+    }
+}
